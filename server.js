c587cf4a3a4721ac2a0a421f41733ecfd54ab223
DannTheWIld <o.mischou@gmail.com>
2025-03-13 07:17:00 +0000

    full

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+app.use(express.json());
+
+let displayData = { text: "", image: "", animation: "fade", players: {}, maxHealth: 5, questions: [], currentQuestionIndex: -1 };
+
+app.post("/update", (req, res) => {
+    displayData = req.body;
+    fs.writeFileSync("questions.json", JSON.stringify(displayData.questions));
+    res.sendStatus(200);
+});
+
+app.get("/data", (req, res) => {
+    res.json(displayData);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:3000/display.html`);
+    console.log(`Server running at http://localhost:3000/control_panel.html`);
+    console.log("Use https://congenial-memory-5gj5xqg4rqvh4p4q-3000.app.github.dev/display.html if working in GitHub Codespaces.");
+    console.log("Use https://congenial-memory-5gj5xqg4rqvh4p4q-3000.app.github.dev/control_panel.html if working in GitHub Codespaces.");
+    console.log("Press Ctrl+C to stop.");
+});
